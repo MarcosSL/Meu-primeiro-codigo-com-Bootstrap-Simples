900d156901ff7fc074eb09202c108f8110edc9c7
MarcosSL <83379537+MarcosSL@users.noreply.github.com>
2021-09-02 22:13:42 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+function calcular(tipo,valor){
+    if(tipo === "acao"){
+      if(valor === "c"){
+        document.getElementById("resultado").value = ""
+      }
+      if(valor === "/" || valor === "-" || valor === "+" || valor === "*"){
+        document.getElementById("resultado").value += valor
+    }
+      if(valor === "="){
+      var res = eval(document.getElementById("resultado").value)
+      document.getElementById("resultado").value = res
+    }
+    }else if(tipo === "valor"){
+      document.getElementById("resultado").value += valor
+    }
+  }
